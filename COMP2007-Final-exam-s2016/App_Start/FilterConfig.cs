59e337382281d4402bac3c24dc15cb5c66dabd25
jasssim2nine <jasim.khan89@gmail.com>
2016-08-18 10:32:11 -0400

    Add project files.

---
+++ COMP2007-Final-exam-s2016/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace COMP2007_Final_exam_s2016
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
