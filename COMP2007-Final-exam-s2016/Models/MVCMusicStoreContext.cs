be34e65f8e7bce4ceb8bec28121e01ec013609bb
jasssim2nine <jasim.khan89@gmail.com>
2016-08-18 10:47:07 -0400

    added mvcstorecontext

---
+++ COMP2007-Final-exam-s2016/Models/MVCMusicStoreContext.cs
@@ -0,0 +1,26 @@
+namespace COMP2007_Final_exam_s2016.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class MVCMusicStoreContext : DbContext
+    {
+        public MVCMusicStoreContext()
+            : base("name=MVCMusicStoreContext")
+        {
+        }
+
+        public virtual DbSet<Album> Albums { get; set; }
+        public virtual DbSet<Artist> Artists { get; set; }
+        public virtual DbSet<Cart> Carts { get; set; }
+        public virtual DbSet<Genre> Genres { get; set; }
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
